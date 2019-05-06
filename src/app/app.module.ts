f3d79a4821ba2e91903ff04f67a599a0ae4978f7
iankurao <kuraoian@gmail.com>
2019-05-07 01:01:33 +0300

    add Http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DisplayComponent } from './display/display.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
